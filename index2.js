a7ed77fa0efbf5ad025f8818b7f0bcc20f43b1e6
Unknown <pratama.naufal@gmail.com>
2018-06-29 13:18:05 +0700

    Added uppercase validation

--- index2.js
+++ index2.js
@@ -4,98 +4,105 @@ var express = require('express');
 var app = express();
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://myapp-df0fa.firebaseio.com/"
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://myapp-df0fa.firebaseio.com/"
 });
 
 var db = admin.database();
 var ref = db.ref("Cities");
 
 app.post('/Cities', function(req, res) {
-    req.on('data', function(data) {
-	datain = JSON.parse(data);
-	ref.once("value", function(snapshot) {
-	    
+	req.on('data', function(data) {
+		datain = JSON.parse(data);
+		if (!(datain['nama'].toUpperCase() === datain['nama']) ||
+			!(datain['negara'].toUpperCase() === datain['negara'])) {
+			result = {
+				"isSuccess": false,
+				"err": 'Nama kota dan negara harus uppercase'
+			}
+			res.send(result);
+			return;
+		} else {
+			ref.once("value", function(snapshot) {
+				var s = "000";
+				s = s + snapshot.numChildren();
+				s = s.substr(s.length-3);
+				var cityRef = ref.child(s);
+				cityRef.set(datain);
+				result = {
+					"isSuccess": true,
+					"data": datain,
+					"ref id": s
+				}
+				res.send(result);
+				console.log('Success adding data');
+			});
+		}
 	});
-	ref.once("value", function(snapshot) {
-	    var s = "000";
-	    s = s + snapshot.numChildren();
-	    s = s.substr(s.length-3);
-	    var cityRef = ref.child(s);
-	    cityRef.set(datain);
-	    result = {
-		"isSuccess": true,
-		"data": datain,
-		"ref id": s
-	    }
-	    res.send(result);
-	    console.log('Success adding data');
-	});
-    });
 });
 
 app.put('/Cities/:id', function(req, res) {
-    req.on('data', function(data) {
-	cityRef = ref.child(req.params.id);
-	datain = JSON.parse(data);
-	cityRef.update(datain);
-	cityRef.once("value", function(snapshot) {
-	    result = {
-	        "isSuccess": true,
-	        "data": snapshot
-	    }
-	    res.send(result);
-	    console.log('Success updating data');
-        });
-    });
+	req.on('data', function(data) {
+		cityRef = ref.child(req.params.id);
+		datain = JSON.parse(data);
+		cityRef.update(datain);
+		cityRef.once("value", function(snapshot) {
+			result = {
+				"isSuccess": true,
+				"data": snapshot
+			}
+			res.send(result);
+			console.log('Success updating data');
+		});
+	});
 });
 
 app.get('/', function(req, res) {
-    res.send("Hello!");
+	res.send("Hello!");
 });
 
 app.get('/Cities', function(req, res) {
-    ref.orderByKey().once("value", function(snapshot) {
-	var data = [];
-	snapshot.forEach(doc => {
-	    data.push(doc);
+	ref.orderByKey().once("value", function(snapshot) {
+		var data = [];
+		snapshot.forEach(doc => {
+			data.push(doc);
+		});
+		result = {
+			"isSuccess": true,
+			"data": data
+		}
+		res.send(result);
 	});
-	result = {
-	    "isSuccess": true,
-	    "data": data
-	}
-	res.send(result);
-    });
 });
 
 app.get('/Cities/:id', function(req, res) {
-    cityRef = ref.child(req.params.id);
-    cityRef.once("value", function(snapshot) {
-	result = {
-	    "isSuccess": true,
-	    "data": snapshot
-	}
-	res.send(result);
-    });
+	cityRef = ref.child(req.params.id);
+		cityRef.once("value", function(snapshot) {
+		result = {
+			"isSuccess": true,
+			"data": snapshot
+		}
+		res.send(result);
+	});
 })
 
 app.delete('/Cities/:id', function(req, res) {
-    cityRef = ref.child(req.params.id);
-    cityRef.remove();
-    ref.orderByKey().once("value", function(snapshot) {
-	var data = [];
-	snapshot.forEach(doc => {
-	    data.push(doc);
+	cityRef = ref.child(req.params.id);
+	cityRef.remove();
+	ref.orderByKey().once("value", function(snapshot) {
+		var data = [];
+		snapshot.forEach(doc => {
+			data.push(doc);
+		});
+		result = {
+			"isSuccess": true,
+			"data": data
+		}
+		res.send(result);
+		console.log('Success deleting data');
 	});
-	result = {
-	    "isSuccess": true,
-	    "data": data
-	}
-	res.send(result);
-	console.log('Success deleting data');
-    });
 })
 
 var server = app.listen(3000, function() {
-    console.log('Sample app is listening...');
+	console.log('Sample app is listening...');
 });
